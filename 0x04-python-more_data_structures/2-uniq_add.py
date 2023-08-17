49495328bb8260d651e4758bb1e7a72e2fb2018a
Isaackaayyy <kammaokoh@Kays-Computer.localdomain>
2023-08-17 01:30:43 +0100

    Update

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def uniq_add(my_list=[]):
     unique_int = set()
     total = 0
 
     for num in my_list:
-    if num not in unique_int:
-        total += num
+        if num not in unique_int:
+            total += num
             unique_int.add(num)
 
     return total
