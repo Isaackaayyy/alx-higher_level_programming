cb18c0045261e806bac3f6690fbd431b6c913b26
Isaackaayyy <kammaokoh@Kays-Computer.localdomain>
2023-08-30 01:14:22 +0100

    Update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Here, is the header"""
 
 
 class Square:
+    """Definition of class square"""
     def __init__(self, size=0):
+	"""Initializes a square instance"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
